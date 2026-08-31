9cc79ec0814674ef8d618668082a7bf637cd208a
mayar dahnous <mayardh27@gmail.com>
2026-08-31 10:10:04 +0300

    Update index.html

--- products.js
+++ products.js
@@ -54,65 +54,35 @@ const product =[{
 ] 
 
 
+const productsContainer =
+    document.getElementById("productsContainer");
 
 
+    function displayProducts(productsToDisplay) {
 
+    productsContainer.innerHTML = "";
+     productsToDisplay.forEach(product => {
 
+        const card = document.createElement("div");
 
+        card.classList.add("product-card");
 
-=======
-const product =[{
-    id: 1,
-    name: "  سوار يدوي بتصاميم تراثية",
-    price: 8,
-    category: "اكسسوارات",
-    Image: "./imgs/سوار.jfif",
-    description: "سوار مستوحاة من النقش الفلسطيني العربي الاصيل ",
+        card.innerHTML = `
+            <img src="${product.image}" alt="${product.name}">
 
-},{
-    id:2,
-    name: "كوب خزفي بتصميم الزيتون",
-    price: 10,
-    category: "هدايا",
-    Image: "./imgs/Handmade Nature Inspired Mug.jfif",
-    description: "كوب خزفي بتصميم الزيتون يتحمل الحرارة ",
+            <h3>${product.name}</h3>
 
+            <p>⭐ ${product.rating}</p>
+
+            <p>${product.description}</p>
+
+            <strong>$${product.price}</strong>
+
+            <button onclick="addToCart(${product.id})">
+                أضف إلى السلة 🛒
+            </button>
+        `;
+
+        productsContainer.appendChild(card);
+    });
 }
-,
-
-{
- id:3,
-    name:" حقيبة قماش مطرزة",
-    price: 18,
-    category: "هدايا",
-    Image: "./imgs/Handmade Nature Inspired Mug.jfif",
-    description: "حقيبة قماش مطرزة مخيطة بخيوط ذاات جودة عالية",
-
-
-}
-,
-{
- id:4,
-    name: "شمعة عطرية",
-    price: 12,
-    category: "هدايا",
-    Image: "./imgs/Vela Aromática Lavanda _ Lumina Collection.jfif",
-    description: "كوب خزفي بتصميم الزيتون يتحمل الحرارة ",
-
-
-}
-,
-{
-    id:5,
-    name: "ثوب فلسطيني ",
-    price: 70,
-    category: "ملابس",
-    Image: "./imgs/download (3).jpg",
-    description: "ثوب فلسطيني ابيض واحمر بجودة عالية onesize",
-
-
-}
-
-] 
-
->>>>>>> 1facf154e32f1787dcbb08554e475bfe5a07d171
