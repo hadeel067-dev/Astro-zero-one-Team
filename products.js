8d19b831d1bc28442eb93a1db325ea82cdbf197d
Mayar Dahnous <mayardh27@gmail.com>
2026-08-31 14:20:48 +0300

    Update products.js

--- products.js
+++ products.js
@@ -54,30 +54,32 @@ const productsContainer =
     document.getElementById("productsContainer");
 
 
-    function displayProducts(productsToDisplay) {
+function displayProducts(productsToDisplay) {
 
-const productsContainer = document.getElementById("productsContainer");
- function displayProducts(productsToDisplay)
-  { productsContainer.innerHTML = ""; 
-    productsToDisplay.forEach(product => { const card = document.createElement("div"); 
-    card.classList.add("product-card");
-     card.innerHTML = ` 
+    const productsContainer = document.getElementById("productsContainer");
+    function displayProducts(productsToDisplay) {
+        productsContainer.innerHTML = "";
+        productsToDisplay.forEach(product => {
+            const card = document.createElement("div");
+            card.classList.add("product-card");
+            card.innerHTML = ` 
      <img src="${product.Image}" alt="${product.name}">
       <h3>${product.name}</h3>
        <p>${product.description}</p>
         <strong>$${product.price}</strong>
 
          <button class="add-to-card" onclick="addToCart(${product.id})"> أضف إلى السلة <i class="fa-solid fa-cart-shopping" style="color: rgb(255, 255, 255);"> </i> </button> `;
-          console.log(card);
-           productsContainer.appendChild(card);
-         }); }
-          displayProducts(products);
+            console.log(card);
+            productsContainer.appendChild(card);
+        });
+    }
+    displayProducts(products);
 
-         <button class="add-to-card" onclick="addToCart(${product.id})"> أضف إلى السلة <i class="fa-solid fa-cart-shopping" style="color: rgb(255, 255, 255);"> </i> </button> ;
-         <button class="removefromcard" onclick="removefromcart(${product.id})"> احذف من السلة <i class="fa-solid fa-cart-shopping" style="color: rgb(255, 255, 255);"> </i> </button> `;
-        console.log(card);
-        productsContainer.appendChild(card);
-    })};
 }
-displayProducts(products);
 
+//     <button class="add-to-card" onclick="addToCart(${product.id})"> أضف إلى السلة <i class="fa-solid fa-cart-shopping" style="color: rgb(255, 255, 255);"> </i> </button>;
+//     <button class="removefromcard" onclick="removefromcart(${product.id})"> احذف من السلة <i class="fa-solid fa-cart-shopping" style="color: rgb(255, 255, 255);"> </i> </button> `;
+//         console.log(card);
+//         productsContainer.appendChild(card);
+//     })};
+// }
