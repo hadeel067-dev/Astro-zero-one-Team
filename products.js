37ed0a774cd71c83880a333dfcd06d87cb14d937
mayar dahnous <mayardh27@gmail.com>
2026-08-31 14:14:17 +0300

    Update index.html

--- products.js
+++ products.js
@@ -47,40 +47,6 @@ const products = [
 ];
 
 
-{
- id:3,
-    name:" حقيبة قماش مطرزة",
-    price: 18,
-    category: "هدايا",
-    image: "./imgs/Handmade Nature Inspired Mug.jfif",
-    description: "حقيبة قماش مطرزة مخيطة بخيوط ذاات جودة عالية",
-
-
-}
-,
-{
- id:4,
-    name: "شمعة عطرية",
-    price: 12,
-    category: "هدايا",
-    image: "./imgs/Vela Aromática Lavanda _ Lumina Collection.jfif",
-    description: "كوب خزفي بتصميم الزيتون يتحمل الحرارة ",
-
-
-}
-,
-{
- id:5,
-    name: "ثوب فلسطيني ",
-    price: 70,
-    category: "ملابس",
-    image: "./imgs/download (3).jpg",
-    description: "ثوب فلسطيني ابيض واحمر بجودة عالية onesize",
-
-
-}
-
-] 
 
 
 
@@ -88,30 +54,32 @@ const productsContainer =
     document.getElementById("productsContainer");
 
 
-    function displayProducts(productsToDisplay) {
+function displayProducts(productsToDisplay) {
 
-const productsContainer = document.getElementById("productsContainer");
- function displayProducts(productsToDisplay)
-  { productsContainer.innerHTML = ""; 
-    productsToDisplay.forEach(product => { const card = document.createElement("div"); 
-    card.classList.add("product-card");
-     card.innerHTML = ` 
+    const productsContainer = document.getElementById("productsContainer");
+    function displayProducts(productsToDisplay) {
+        productsContainer.innerHTML = "";
+        productsToDisplay.forEach(product => {
+            const card = document.createElement("div");
+            card.classList.add("product-card");
+            card.innerHTML = ` 
      <img src="${product.Image}" alt="${product.name}">
       <h3>${product.name}</h3>
        <p>${product.description}</p>
         <strong>$${product.price}</strong>
 
          <button class="add-to-card" onclick="addToCart(${product.id})"> أضف إلى السلة <i class="fa-solid fa-cart-shopping" style="color: rgb(255, 255, 255);"> </i> </button> `;
-          console.log(card);
-           productsContainer.appendChild(card);
-         }); }
-          displayProducts(products);
+            console.log(card);
+            productsContainer.appendChild(card);
+        });
+    }
+    displayProducts(products);
 
-         <button class="add-to-card" onclick="addToCart(${product.id})"> أضف إلى السلة <i class="fa-solid fa-cart-shopping" style="color: rgb(255, 255, 255);"> </i> </button> ;
-         <button class="removefromcard" onclick="removefromcart(${product.id})"> احذف من السلة <i class="fa-solid fa-cart-shopping" style="color: rgb(255, 255, 255);"> </i> </button> `;
+    <button class="add-to-card" onclick="addToCart(${product.id})"> أضف إلى السلة <i class="fa-solid fa-cart-shopping" style="color: rgb(255, 255, 255);"> </i> </button>;
+    <button class="removefromcard" onclick="removefromcart(${product.id})"> احذف من السلة <i class="fa-solid fa-cart-shopping" style="color: rgb(255, 255, 255);"> </i> </button> `:
         console.log(card);
         productsContainer.appendChild(card);
     })};
-}
-displayProducts(products);
 
+displayProducts(products);
+        
