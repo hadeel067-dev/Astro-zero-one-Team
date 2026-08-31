722da8a5b3511c2a35f0b284f43b839f27418f36
Hadeel Taha <thadeel067@gmail.com>
2026-08-31 11:45:48 +0300

    anything

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
+// const searchInput = document.querySelector("#search-box");
+// searchInput.addEventListener("input",
+//      () => {
+//           const searchValue = searchInput.value.trim().toLowerCase();
+//           const results = products.filter(product => product.name.toLowerCase().includes(searchValue)); displayProducts(results);
+//      });
+// const categoryButtons = document.querySelectorAll(".category-btn");
+// categoryButtons.forEach(button => {
+//      button.addEventListener("click",
+//           () => {
+//                const category = button.dataset.category;
+//                if (category === "الكل") {
+//                     displayProducts(products);
 
-const searchinput =document.querySelector('#search-box');
+//                } else {
+//                     const results = products.filter(product => product.category === category);
+//                     displayProducts(results);
+//                }
+//           });
+// });
+const searchinput =document.querySelector("#search-box");
 searchinput.addEventListener("input",()=>{
-    const searchvalue=searchinput.textContent;
+    const searchvalue=searchinput.value;
      const result =product.filter(product =>
 product.name
 .includes(searchvalue)
@@ -9,7 +28,7 @@ product.name
      displayProducts(result);
 })
 
-const categoryButtons=document.querySelectorAll('.category-btn');
+const categoryButtons=document.querySelectorAll(".category-btn");
  categoryButtons.forEach(div =>{
  div.addEventListener("click",()=>{
      
